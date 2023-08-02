f25eb7dca2ca25d2248dfdda872ddc64b1e71065
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2023-08-02 12:23:12 -0400

    LS- Added Triangulos.py file

---
+++ Triangulos.py
@@ -0,0 +1,42 @@
+def verificarTriangulo(a,b,c):
+    if a == b == c:
+        return "Equilatero"
+    elif(a == b or b == c or c == a):
+        return "Isosceles"
+    else:
+        return "Escaleno"
+    
+def verificarLado(msg):
+    while True:
+        try:
+            lado = int(input(msg))
+            if lado < 0:
+                raise ValueError
+            return lado
+        except ValueError:
+            print("Ingrese un valor positivo")
+        
+def ejecutar():
+    
+    print("Ingrese los lados a, b, c de su triangulo para clasificarlo:")
+        
+    a = verificarLado("Ingrese el lado a: ")
+    b = verificarLado("Ingrese el lado b: ")
+    c = verificarLado("Ingrese el lado c: ")
+
+    print("Su triangulo es: " + verificarTriangulo(a,b,c))    
+    
+    while True:
+        res = input("¿Desea intentar de nuevo? y/n\n")
+    
+        if res.lower() == "y":
+            ejecutar()
+        elif res.lower() == "n":
+            break
+        else:
+            print("Opcion no valida: "+res)
+
+            
+if __name__ == "__main__":
+    ejecutar()
+            
