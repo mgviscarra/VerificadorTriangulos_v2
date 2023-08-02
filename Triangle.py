32b1fcf349e8b9bac6a879f4f2ff9955692b444a
selwavalencia@gmail.com <selwavalencia@gmail.com>
2023-08-02 13:46:11 -0400

    SelwaValencia Added get_float_value() and refactored insert_sides().

--- Triangle.py
+++ Triangle.py
@@ -5,9 +5,9 @@ class Triangle:
         self.sideC = 0
 
     def insert_sides(self):
-        self.sideA = input("INGRESE EL LADO A DEL TRIANGULO:")
-        self.sideB = input("INGRESE EL LADO B DEL TRIANGULO:")
-        self.sideC = input("INGRESE EL LADO C DEL TRIANGULO:")
+        self.sideA = self.get_float_value("INGRESE EL LADO A DEL TRIANGULO:")
+        self.sideB = self.get_float_value("INGRESE EL LADO B DEL TRIANGULO:")
+        self.sideC = self.get_float_value("INGRESE EL LADO C DEL TRIANGULO:")
 
     def get_tiangle_type(self):
         if self.sideA == self.sideB == self.sideC:
@@ -17,6 +17,24 @@ class Triangle:
         else:
             print("EL TRIANGULO ES ESCALENO")
 
-T = Triangle()
-T.insert_sides()
-T.get_tiangle_type()
+    def get_float_value(self, message):
+        while True:
+            try:
+                side = float(input(message))
+                return side
+            except ValueError:
+                print("ERROR: Por favor ingrese un número.")
+
+while True:
+
+    T = Triangle()
+
+    check = input("¿Desea ingresar un triangulo? (s/n): ")
+
+    if check.lower() == "s":
+        T.insert_sides()
+        T.get_tiangle_type()
+    elif check.lower() == "n":
+        break
+    else:
+        print("Error: Ingrese 's' para continuar o 'n' para salir.")
