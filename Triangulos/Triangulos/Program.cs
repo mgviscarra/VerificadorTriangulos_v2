cc4a51c705f42100e0644d2ce466deb1d5c4d26b
Gabriel Villarreal Ponce <gabriel.villarreal.ponce@gmail.com>
2023-08-02 08:45:46 -0400

    Triangulos gabrielvillarreal

---
+++ Triangulos/Triangulos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulos triangulos = new Triangulos();
+            Console.WriteLine("INGRESE EL LADO A DEL TRIANGULO:");
+            int ladoA = int.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE EL LADO B DEL TRIANGULO:");
+            int ladoB = int.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE EL LADO C DEL TRIANGULO:");
+            int ladoC = int.Parse(Console.ReadLine());
+            Console.WriteLine(triangulos.CalcularTipo(ladoA,ladoB,ladoC));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Triangulos/Triangulos/Triangulos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos
+{
+    internal class Triangulos
+    {
+        int ladoA;
+        int ladoB;
+        int ladoC;  
+        public string CalcularTipo(int ladoA, int ladoB, int ladoC)
+        {
+            this.ladoA = ladoA;
+            this.ladoB = ladoB;
+            this.ladoC = ladoC;
+            if (ladoA == ladoB && ladoB == ladoC)
+            {
+                return "EL TRIANGULO ES EQUILATERO";
+            }
+            else if (ladoA == ladoB || ladoA == ladoC || ladoB == ladoC)
+            {
+                return "EL TRIANGULO ES ISOSCELES";
+            }
+            else 
+            {
+                return "EL TRIANGULO ES ESCALENO";
+            }
+        }
+    }
+}
