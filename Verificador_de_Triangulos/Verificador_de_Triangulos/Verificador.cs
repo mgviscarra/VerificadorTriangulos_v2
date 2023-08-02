6ef3d6a3441804f50b6013b8da3b68b8b492f7fa
SantiAlmancy <santialmancy@gmail.com>
2023-08-02 15:14:35 -0400

    SantiagoBecerra Added methods DeterminarTriangulo and ValidarTriangulo in Verificador.cs.

--- Verificador_de_Triangulos/Verificador_de_Triangulos/Verificador.cs
+++ Verificador_de_Triangulos/Verificador_de_Triangulos/Verificador.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Verificador_de_Triangulos
+﻿namespace Verificador_de_Triangulos
 {
-    internal class Verificador
+    public static class Verificador
     {
+        //Verificacion del tipo del triangulo
+        public static string DeterminarTriangulo(float a, float b, float c)
+        {
+            if (!ValidarTriangulo(a, b, c))
+            {
+                return ("TRIANGULO INVALIDO");
+            }
+
+            if (a == b && b == c)
+            {
+                return ("EL TRIANGULO ES EQUILATERO");
+            }
+            else if (a == b || a == c || b == c)
+            {
+                return ("EL TRIANGULO ES ISOSCELES");
+            }
+            else
+            {
+                return ("EL TRIANGULO ES ESCALENO");
+            }
+        }
+
+        // Verificacion de validez de los valores a, b y c
+        private static bool ValidarTriangulo(float a, float b, float c)
+        {
+            return (a + b > c && a + c > b && b + c > a);
+        }
     }
 }
