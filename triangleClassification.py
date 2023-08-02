ec89403ac557d2d15255c4575dda56458b4c57d6
Mateo Mercado <matemerc@gmail.com>
2023-08-02 11:52:24 -0400

    Tried to link both files to use methods but there is bug that makes to run original file.

--- triangleClassification.py
+++ triangleClassification.py
@@ -1,18 +1,19 @@
-from trianglesCoornidates import classifyTriangles
+from trianglesCoordinates import classifyTriangles
 
-def main():
-    print('Introduce la distancia 1:')
-    dist1 = input()
-    print('Introduce la distancia 2:')
-    dist2 = input()
-    print('Introduce la distancia 3:')
-    dist3 = input()
+class tringleType:
+    def main():
+        print('Introduce la distancia 1:')
+        dist1 = input()
+        print('Introduce la distancia 2:')
+        dist2 = input()
+        print('Introduce la distancia 3:')
+        dist3 = input()
 
-    if (classifyTriangles.isEquilateral(dist1, dist2, dist3)):
-        print('El triangulo es equilatero')
-    elif(classifyTriangles.isEscalene(dist1, dist2, dist3)):
-        print('El triangulo es escaleno')
-    else:
-        print('El triangulo es isosceles')
+        if (tringleType.classifyTriangles.isEquilateral(dist1, dist2, dist3)):
+            print('El triangulo es equilatero')
+        elif(tringleType.classifyTriangles.isEscalene(dist1, dist2, dist3)):
+            print('El triangulo es escaleno')
+        else:
+            print('El triangulo es isosceles')
 
-classifyClassification.main()
+tringleType.main()
