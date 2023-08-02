24514677d3d2dc7fb70a201f1ad4a8c337d65b0e
SantiAlmancy <santialmancy@gmail.com>
2023-08-02 15:18:55 -0400

    SantiagoBecerra Modified Program.cs.

--- Verificador_de_Triangulos/Verificador_de_Triangulos/Program.cs
+++ Verificador_de_Triangulos/Verificador_de_Triangulos/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace Verificador_de_Triangulos
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                try
+                {
+                    float a = Verificador.NumeroValido("INGRESE EL LADO A DEL TRIANGULO:");
+                    float b = Verificador.NumeroValido("INGRESE EL LADO B DEL TRIANGULO:");
+                    float c = Verificador.NumeroValido("INGRESE EL LADO C DEL TRIANGULO:");
+
+                    Console.WriteLine(Verificador.DeterminarTriangulo(a, b, c));
+                }
+                // Errores inesperados
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"ERROR INESPERADO: {ex.Message}");
+                }
+                //Control del ciclo
+                Console.WriteLine("¿DESEA VOLVER A INTENTAR VERIFICAR UN TRIANGULO? (S/Cualquier otra respuesta)");
+                string respuesta = Console.ReadLine();
+                if (!respuesta.Equals("S", StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+            }
         }
     }
 }
