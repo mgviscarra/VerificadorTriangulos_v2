20614b5ba8b93a6f1d9f846f68f08f71aff26c37
Laura Delgadillo <delgadilloarterolaura@gmail.com>
2023-08-02 14:18:52 -0400

    LauraDelgadillo Added checkTriangle method to validate the input side lengths for positive values and correct triangle. Modified main method for input values from the console.

--- VerificadorTriangulos.py
+++ VerificadorTriangulos.py
@@ -7,9 +7,24 @@ class VerificadorTriangulos:
         else:
             return "Escaleno"
         
+    def checkTriangle(self, a, b, c):
+        if a <= 0 or b <= 0 or c <= 0:
+            print("\nError: Los lados del triangulo deben ser numeros positivos.\n")
+            return False
+        elif a + b <= c or a + c <= b or b + c <= a:
+            print("\nError: La suma de dos lados del triangulo debe ser mayor al tercer lado.\n")
+            return False
+        else:
+            return True
+        
     def main(self):
-        triangle = self.triangleType(7, 8, 9)
-        print("El triangulo es " + triangle + "\n")
+        a = float(input("Ingrese el lado A del triangulo: "))
+        b = float(input("Ingrese el lado B del triangulo: "))
+        c = float(input("Ingrese el lado C del triangulo: "))
+        
+        if self.checkTriangle(a, b, c):
+            triangle_type = self.triangleType(a, b, c)
+            print("El triangulo es " + triangle_type + "\n")
 
 if __name__ == "__main__":
     verificadorTriangulos = VerificadorTriangulos()
